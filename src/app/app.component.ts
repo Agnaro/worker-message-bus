4f86f24da95774020566d53f5c009145a1ded589
Eric Root <e70347@gmail.com>
2020-07-18 16:23:44 -0400

    start example app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { WorkerMessageBus } from '../message-bus/worker-message-bus';
+import { withLatestFrom, map } from 'rxjs/operators';
+import { Message } from 'src/message-bus/messages/message';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'worker-message-bus';
+  get maxVal(): number {
+    return this._maxVal.getValue();
+  }
+
+  set maxVal(v: number) {
+    this._maxVal.next(v);
+  }
+
+  get output(): string {
+    return this._output.getValue();
+  }
+
+  private _maxVal = new BehaviorSubject<number>(1);
+  private _output = new BehaviorSubject<string>('');
+
+  private worker = new Worker('../workers/example-worder.worker.js');
+  private workerBus = new WorkerMessageBus(
+    this.worker.postMessage,
+    this.worker
+  );
+
+  private startCalculation$ = new Subject<void>();
+
+  private calculationRequests$ = this.startCalculation$.pipe(
+    withLatestFrom(this._maxVal, (_, maxVal) => new Message())
+  );
+
+  public onBtnClick() {
+    this.startCalculation$.next();
+  }
 }
